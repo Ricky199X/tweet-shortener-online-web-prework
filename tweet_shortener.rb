cc5f394af51f36a35a5a067cb9d04eb0c78bc50c
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-30 15:44:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+def word_substituter(string)
+  hash_values = hash.keys
+  str_array = string.split(' ')
+    str_array.map {|i| i == hash.values ? i == dictionary[:value] : i}
+      end
