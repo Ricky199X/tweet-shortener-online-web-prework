0366829453f81661d8a2a1a7b3122987d01e3a65
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-30 15:59:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,9 +31,6 @@ end
 
 def selective_tweet_shortener(tweets)
   tweets.each do |tweet|
-    if tweet.length > 140
-      word_substituter(tweet)
-      else
-    tweet
+    tweet.length > 140 ? word_substituter(tweet) : tweet
   end
 end
