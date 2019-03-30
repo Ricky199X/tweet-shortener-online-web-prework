f78f0ed9d9bb0da58f10ff4169b00e4293a6eff8
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-30 15:45:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,17 @@
 def word_substituter(tweet)
+  dictionary = {
+  "hello" => "hi",
+  "two" => "2",
+  "to" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+
   tweet.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
